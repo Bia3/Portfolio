f53d9986398f48e69abe640c7b785dd71b959da7
Sean Ross <sean.r.ross@gmail.com>
2019-11-08 02:36:08 -0700

    Settings Update

--- Portfolio/heroku_settings.py
+++ Portfolio/heroku_settings.py
@@ -3,9 +3,9 @@ import django_heroku
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-MIDDLEWARE = []
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+# MIDDLEWARE = []
 
 settings.DEBUG = False
 settings.TEMPLATE_DEBUG = False
@@ -15,16 +15,11 @@ settings.SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # # Database
 # # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydatabase',
-#         'USER': 'mydatabaseuser',
-#         'PASSWORD': 'mypassword',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+    }
+}
 #
 # settings.DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 # # Internationalization
