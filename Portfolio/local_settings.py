1ccac52badcfc55c0710ac549627cc0c1b0dfec8
Sean Ross <sean.r.ross@gmail.com>
2020-01-09 09:36:22 -0700

    Adding local_settings.py

---
+++ Portfolio/local_settings.py
@@ -0,0 +1,13 @@
+from . import settings
+import os
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+settings.DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(settings.BASE_DIR, 'db.sqlite3'),
+    }
+}
+
