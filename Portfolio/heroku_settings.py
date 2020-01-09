41c8b6a779a2452eeb65028a9a3c41001a885873
Sean Ross <sean.r.ross@gmail.com>
2020-01-09 10:24:07 -0700

    Removing SSL middleware

--- Portfolio/heroku_settings.py
+++ Portfolio/heroku_settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-MIDDLEWARE.append('RossDev.middleware.SSLMiddleware')
+# MIDDLEWARE.append('RossDev.middleware.SSLMiddleware')
 
 # # Database
 # # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
