505cf6d62c168aa7ac72841f74836a4625c88b8c
Sean <ltuae@pm.me>
2020-03-10 07:26:53 -0600

    adjusting allowed hosts

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -116,9 +116,9 @@ DEV_BIT = os.getenv('DEV_BIT') is not None
 HEROKU_DEV_BIT = os.getenv('HEROKU_DEV_BIT') is not None
 
 # Overwrite settings for security in Production and Live Sever Testing Environments.
-# if not DEV_BIT:
-#     from .heroku_settings import *
-# elif HEROKU_DEV_BIT:
-#     from .heroku_dev_settings import *
-# else:
-#     from .local_settings import *
+if not DEV_BIT:
+    from .heroku_settings import *
+elif HEROKU_DEV_BIT:
+    from .heroku_dev_settings import *
+else:
+    from .local_settings import *
