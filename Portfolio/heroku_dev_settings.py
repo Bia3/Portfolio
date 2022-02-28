e630252b88080c0fdbc2aca65c121499e278229c
Sean Ross <sean@aphid.io>
2022-02-27 17:30:43 -0700

    Fixed Heroku Settings

--- Portfolio/heroku_dev_settings.py
+++ Portfolio/heroku_dev_settings.py
@@ -14,23 +14,23 @@ from . import settings
 import dj_database_url
 import os
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+settings.DEBUG = True
+settings.TEMPLATE_DEBUG = True
 
 # ToDo: Finish setting up Heroku Allowed Hosts for Dev
 # ALLOWED_HOSTS = [
 #     '127.0.0.1', 'localhost', 'portdev.herokuapp.com'
 # ]
-ALLOWED_HOSTS = ['*']
+settings.ALLOWED_HOSTS = ['*']
 # Use the production secret key
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+settings.SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # Security settings https://docs.djangoproject.com/en/3.2/topics/security/
 # SSL/HTTPS
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+settings.SECURE_SSL_REDIRECT = True
+settings.SESSION_COOKIE_SECURE = True
+settings.CSRF_COOKIE_SECURE = True
 
-DATABASE_URL = os.environ['DATABASE_URL']
+settings.DATABASE_URL = os.environ['DATABASE_URL']
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+settings.DATABASES = {'default': dj_database_url.config(default=settings.DATABASE_URL)}
--- Portfolio/heroku_settings.py
+++ Portfolio/heroku_settings.py
@@ -14,26 +14,26 @@ from . import settings
 import dj_database_url
 import os
 
-DEBUG = False
-TEMPLATE_DEBUG = True
+settings.DEBUG = False
+settings.TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = [
+settings.ALLOWED_HOSTS = [
     'www.rossdev.io', 'www.rossdev.tech', 'www.rossdev.co', 'rossdev.io', 'rossdev.tech', 'rossdev.co',
     'portf-orchard.herokuapp.com', '1276.0.0.1', 'localhost'
 ]
 
 # Use the production secret key
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+settings.SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-DATABASE_URL = os.environ['DATABASE_URL']
+settings.DATABASE_URL = os.environ['DATABASE_URL']
 
 # Security settings https://docs.djangoproject.com/en/3.2/topics/security/
 # SSL/HTTPS
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+settings.SECURE_SSL_REDIRECT = True
+settings.SESSION_COOKIE_SECURE = True
+settings.CSRF_COOKIE_SECURE = True
 
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+settings.DATABASES = {'default': dj_database_url.config(default=settings.DATABASE_URL)}
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
