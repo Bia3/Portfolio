b49988656ff245e7f0cc86bde7996a746f2180a2
Sean Ross <sean.r.ross@gmail.com>
2022-02-28 16:57:00 +0000

    Added back in the helper template tags, added back in the header and footer templates for large header/footer

---
+++ portfolio/templatetags/my_helpers.py
@@ -0,0 +1,32 @@
+from django import template
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+from ..models import Svg
+
+register = template.Library()
+
+
+@register.simple_tag
+def svg_by_name(svg_name):
+    try:
+        svg = Svg.objects.get(name=svg_name)
+        return mark_safe("{}".format(svg.data))
+    except Svg.DoesNotExist:
+        return mark_safe("<span>Unable to find SVG</span>")
+
+
+@register.simple_tag
+def footer_icon(txt, link, svg_name):
+    templ = 'footer_ico_link.html'
+    context = {
+        'txt': txt,
+        'link': link,
+        'svg_name': svg_name
+    }
+    resp = render(request=None, template_name=templ, context=context)
+    resp = render_to_string(template_name=templ, context=context)
+    return mark_safe(resp)
+
+    # return f'{txt}, {link}, {svg_name}'
