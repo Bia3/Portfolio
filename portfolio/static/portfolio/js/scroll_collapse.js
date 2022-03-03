1952b1177dbc593093acbf4fa9ecde62f03e5c06
Sean Ross <sean.r.ross@gmail.com>
2022-03-02 18:51:38 -0700

    Smooth scroll to top at beginning of collapse

--- portfolio/static/portfolio/js/scroll_collapse.js
+++ portfolio/static/portfolio/js/scroll_collapse.js
@@ -68,6 +68,7 @@ function enableScroll() {
 // on scroll
 document.addEventListener("scroll", () => {
   if (collapse_header){
+    window.scrollTo({top: 0, behavior: 'smooth'});
     setTimeout(() => {
       setTimeout(() => {
         document.querySelector("#StandardHeader").classList.add("visible");
