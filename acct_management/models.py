14cfb24cb36015fb6e37d45386ac4e38223a0dd2
Sean Ross <sean.r.ross@gmail.com>
2022-03-11 22:56:04 +0000

    Models for acct_managment

--- acct_management/models.py
+++ acct_management/models.py
@@ -1,2 +1,44 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
 
-# Create your models here.
+
+class ContactCard(models.Model):
+    """
+    Model for a contact card that has a one to many relationship with
+    User.
+    Posibly use https://django-fernet-fields.readthedocs.io/en/latest/ or
+    https://pypi.org/project/django-encrypted-model-fields/ to encypt
+    phone
+    """
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    website = models.CharField(max_length = 250)
+    # phone = models.CharField(max_length = 20)
+
+
+class Bio(models.Model):
+    """A paragraph about the User"""
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    profession = models.CharField(max_length = 250)
+    copy = MarkdownxField(max_length = 3000)
+
+
+class Address(models.Model):
+    """"""
+    id = models.UUIDField(
+        primar_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete = models.CASCADE)
