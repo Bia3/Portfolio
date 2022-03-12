58c344ae9c24bc6f1e31e2be8c7cabf3fc780c75
Sean Ross <sean@aphid.io>
2022-03-12 11:22:01 -0700

    acct_management app database struct

--- acct_management/models.py
+++ acct_management/models.py
@@ -2,43 +2,115 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
+import secured_fields
 
 
 class ContactCard(models.Model):
-    """
-    Model for a contact card that has a one to many relationship with
-    User.
-    Posibly use https://django-fernet-fields.readthedocs.io/en/latest/ or
-    https://pypi.org/project/django-encrypted-model-fields/ to encypt
-    phone
-    """
+    """Model for a contact card that has a one to many relationship with User."""
     id = models.UUIDField(
-        primary_key = True,
-        default = uuid.uuid4,
-        editable = False
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
     )
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    website = models.CharField(max_length = 250)
-    # phone = models.CharField(max_length = 20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    website = models.CharField(max_length=250)
+    phone = secured_fields.EncryptedCharField(max_length=20)
 
 
 class Bio(models.Model):
     """A paragraph about the User"""
     id = models.UUIDField(
-        primary_key = True,
-        default = uuid.uuid4,
-        editable = False
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
     )
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    profession = models.CharField(max_length = 250)
-    copy = MarkdownxField(max_length = 3000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profession = models.CharField(max_length=250)
+    copy = MarkdownxField(max_length=3000)
 
 
 class Address(models.Model):
-    """"""
+    """A set of protected fields for downloaded versions of the CV and Resume"""
     id = models.UUIDField(
-        primar_key = True,
-        default = uuid.uuid4,
-        editable = False
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
     )
-    contact_card = models.ForeignKey(ContactCard, on_delete = models.CASCADE)
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE, unique=True)
+    street_one = secured_fields.EncryptedCharField(max_length=250)
+    street_two = secured_fields.EncryptedCharField(max_length=250)
+    city = secured_fields.EncryptedCharField(max_length=180)
+    state = secured_fields.EncryptedCharField(max_length=180)
+    zip = secured_fields.EncryptedCharField(max_length=10)
+
+
+class GitHub(models.Model):
+    """GitHub profile information"""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE)
+    username = models.CharField(max_length=180)
+    link = models.CharField(max_length=250)
+
+
+class Keybase(models.Model):
+    """Keybase profile information"""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE)
+    username = models.CharField(max_length=180)
+    link = models.CharField(max_length=250)
+
+
+class Codewars(models.Model):
+    """Codewars profile information"""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE)
+    username = models.CharField(max_length=180)
+    link = models.CharField(max_length=250)
+
+
+class LinkedIn(models.Model):
+    """LinkedIn profile information"""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE)
+    username = models.CharField(max_length=180)
+    link = models.CharField(max_length=250)
+
+
+class HackTheBox(models.Model):
+    """HackTheBox profile information"""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE)
+    username = models.CharField(max_length=180)
+    link = models.CharField(max_length=250)
+
+
+class CodePen(models.Model):
+    """CodePen profile information"""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    contact_card = models.ForeignKey(ContactCard, on_delete=models.CASCADE)
+    username = models.CharField(max_length=180)
+    link = models.CharField(max_length=250)
