4071d98220c3fecfc843a9d9d7aa881837b11814
Sean Ross <sean@aphid.io>
2022-03-26 10:16:19 -0600

    Code of Conduct view

--- seo/views.py
+++ seo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+
+class CodeOfConductView(View):
+    template_name = 'code_of_conduct.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            with open('./docs/CODE_OF_CONDUCT.md', 'r') as file:
+                coc = file.read()
+        except FileNotFoundError as error:
+            return HttpResponse(status=500)
+
+        context = {
+            'md': coc
+        }
+
+        return render(request, self.template_name, context)
